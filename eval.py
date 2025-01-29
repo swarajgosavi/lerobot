27ab38120de771142f81d8e3dae56d5d03460594
swarajgosavi <swarajgosavi20@gmail.com>
2025-01-29 12:53:13 +0530

    Extended support for 7 dof kikobot

---
+++ eval.py
@@ -0,0 +1,78 @@
+from lerobot.common.robot_devices.cameras.opencv import OpenCVCamera
+from lerobot.common.robot_devices.utils import busy_wait
+from pathlib import Path
+from huggingface_hub import snapshot_download
+
+from lerobot.common.robot_devices.motors.feetech import FeetechMotorsBus
+follower_port = "/dev/ttyACM0"
+
+follower_arm = FeetechMotorsBus(
+    port=follower_port,
+    motors={
+        # name: (index, model)
+      "shoulder_pan": (1, "sts3215"),
+      "shoulder_lift": (2, "sts3215"),
+      "elbow_flex": (3, "sts3215"),
+      "wrist_flex": (4, "sts3215"),
+      "wrist_roll": (5, "sts3215"),
+      "gripper": (6, "sts3215"),
+    },
+)
+
+# follower_arm.connect()
+
+from lerobot.common.robot_devices.robots.manipulator import ManipulatorRobot
+
+robot = ManipulatorRobot(
+    robot_type="so100",
+    # leader_arms={"main": leader_arm},
+    follower_arms={"main": follower_arm},
+    calibration_dir=".cache/calibration/so100",
+    cameras={
+        "laptop": OpenCVCamera(2, fps=30, width=640, height=480),
+        "phone": OpenCVCamera(4, fps=30, width=640, height=480),
+    },
+)
+# robot.disconnect()
+robot.connect()
+
+from lerobot.common.policies.act.modeling_act import ACTPolicy
+import time
+import torch
+
+inference_time_s = 60
+fps = 30
+device = "cpu"  # TODO: On Mac, use "mps" or "cpu"
+
+ckpt_path = Path(snapshot_download("swarajgosavi/act_kikobot_pusht_real"))
+policy = ACTPolicy.from_pretrained(ckpt_path)
+policy.to(device)
+
+for _ in range(inference_time_s * fps):
+    start_time = time.perf_counter()
+
+    # Read the follower state and access the frames from the cameras
+    observation = robot.capture_observation()
+    print(observation)
+    # Convert to pytorch format: channel first and float32 in [0,1]
+    # with batch dimension
+    for name in observation:
+        if "image" in name:
+            observation[name] = observation[name].type(torch.float32) / 255
+            observation[name] = observation[name].permute(2, 0, 1).contiguous()
+        observation[name] = observation[name].unsqueeze(0)
+        observation[name] = observation[name].to(device)
+
+    # Compute the next action with the policy
+    # based on the current observation
+    with torch.inference_mode():
+        action = policy.select_action(observation)
+    # Remove batch dimension
+    action = action.squeeze(0)
+    # Move to cpu, if not already the case
+    action = action.to("cpu")
+    # Order the robot to move
+    robot.send_action(action)
+
+    dt_s = time.perf_counter() - start_time
+    busy_wait(1 / fps - dt_s)
